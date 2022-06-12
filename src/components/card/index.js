58ea10ac33e931ff54e15392d44b1e6f012af9cc
Ilija Radivojevic <57087903+IlijaRad@users.noreply.github.com>
2022-06-12 22:03:34 +0200

    Added More Info Button

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -10,27 +10,27 @@ const Card = ({
   imgAlt = "",
 }) => {
   return (
-    <a
-      href={websiteLink}
-      class="flex flex-col overflow-hidden shadow-lg rounded-lg bg-gray-100 dark:bg-secondary border border-gray-300 dark:border-gray-600"
+    <div
+      onClick={() => (window.location = websiteLink)}
+      className="flex flex-col overflow-hidden shadow-lg rounded-lg bg-gray-100 dark:bg-secondary border border-gray-300 dark:border-gray-600 cursor-pointer"
     >
-      <div class="flex-shrink-0">
+      <div className="flex-shrink-0">
         <img
-          class="h-[200px] w-full object-cover border-b border-gray-300 dark:border-gray-600"
+          className="h-[200px] w-full object-cover border-b border-gray-300 dark:border-gray-600"
           src={imgSrc}
           alt={imgAlt}
         />
       </div>
-      <div class="flex-1 p-6 flex flex-col justify-between">
-        <div class="flex-1">
-          <h3 class="mt-2 text-xl leading-7 font-semibold text-black dark:text-white">
+      <div className="flex-1 p-6 flex flex-col justify-between">
+        <div className="flex-1">
+          <h3 className="mt-2 text-xl leading-7 font-semibold text-black dark:text-white">
             {title}
           </h3>
-          <p class="mt-3 leading-6 text-base line-clamp-4 text-gray-500 dark:text-gray-dark prose">
+          <p className="mt-3 leading-6 text-base line-clamp-4 text-gray-500 dark:text-gray-dark prose">
             {body}
           </p>
         </div>
-        <div className="mt-6 text-primary flex flex-wrap flex-col xl:flex-row justify-between w-full items-center text-left font-medium focus:outline-none cursor-pointer transition">
+        <div className="mt-6 text-primary flex flex-wrap flex-col xl:flex-row justify-between w-full items-center font-medium focus:outline-none cursor-pointer transition">
           <a
             href={gitHubLink}
             className="icon-parent w-full 2xl:w-auto px-6 py-2 mb-4 flex items-center justify-center text-center rounded-md border dark:border-gray-600 border-gray-300 dark:hover:border-white dark:hover:bg-white dark:hover:text-black hover:bg-gray-900 hover:text-white hover:border-gray-900 transition"
@@ -43,11 +43,11 @@ const Card = ({
             className="icon-parent-stroke w-full 2xl:w-auto px-6 py-2 mb-4 flex items-center justify-center text-center rounded-md border dark:border-gray-600 border-gray-300 dark:hover:border-white dark:hover:bg-white dark:hover:text-black hover:bg-gray-900 hover:text-white hover:border-gray-900  transition"
           >
             <InfoIcon className="h-5 w-5 dark:stroke-white stroke-black mr-2 transition-all" />
-            More info (WIP)
+            More Info
           </a>
         </div>
       </div>
-    </a>
+    </div>
   );
 };
 
