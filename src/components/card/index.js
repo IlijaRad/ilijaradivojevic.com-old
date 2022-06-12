9bbe9e5ec8c0c009a34b8f44c0ccb35a93ff8942
Ilija Radivojevic <57087903+IlijaRad@users.noreply.github.com>
2022-06-12 21:49:37 +0200

    Added More Info button

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,4 +1,5 @@
 import GithubIcon from "../../assets/icons/GithubIcon";
+import InfoIcon from "../../assets/icons/InfoIcon";
 
 const Card = ({
   title,
@@ -29,13 +30,20 @@ const Card = ({
             {body}
           </p>
         </div>
-        <div className="mt-6 text-primary justify-between w-full items-center text-left font-medium focus:outline-none cursor-pointer transition">
+        <div className="mt-6 text-primary flex flex-wrap flex-col xl:flex-row justify-between w-full items-center text-left font-medium focus:outline-none cursor-pointer transition">
           <a
             href={gitHubLink}
-            className="icon-parent w-full px-6 py-2 mb-4 flex items-center justify-center text-center rounded-md border dark:border-gray-600 border-gray-300 dark:hover:border-white dark:hover:bg-white dark:hover:text-black hover:bg-gray-900 hover:text-white hover:border-gray-900 dark:hover:fill-black transition"
+            className="icon-parent w-full 2xl:w-auto px-6 py-2 mb-4 flex items-center justify-center text-center rounded-md border dark:border-gray-600 border-gray-300 dark:hover:border-white dark:hover:bg-white dark:hover:text-black hover:bg-gray-900 hover:text-white hover:border-gray-900 transition"
           >
             <GithubIcon className="h-5 w-5 dark:fill-white fill-black mr-2 transition-all" />
             View source
+          </a>
+          <a
+            href="#/"
+            className="icon-parent-stroke w-full 2xl:w-auto px-6 py-2 mb-4 flex items-center justify-center text-center rounded-md border dark:border-gray-600 border-gray-300 dark:hover:border-white dark:hover:bg-white dark:hover:text-black hover:bg-gray-900 hover:text-white hover:border-gray-900  transition"
+          >
+            <InfoIcon className="h-5 w-5 dark:stroke-white stroke-black mr-2 transition-all" />
+            More info (WIP)
           </a>
         </div>
       </div>
