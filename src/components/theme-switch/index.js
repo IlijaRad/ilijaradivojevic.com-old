e008f3830901e915a290de5c0654cdf1b8dea9d6
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-06-28 17:28:31 +0200

    css refactor

--- src/components/theme-switch/index.js
+++ src/components/theme-switch/index.js
@@ -6,24 +6,22 @@ export default function ThemeSwitch() {
   const { switched, setSwitched } = useContext(ThemeContext);
 
   return (
-    <div>
-      <Switch
-        checked={switched}
-        onChange={() =>
-          setTimeout(() => setSwitched((switched) => !switched), 300)
-        }
-        className={`${
-          !switched ? "bg-gray-900 dark:bg-gray-300/20" : "bg-gray-200"
-        }
+    <Switch
+      checked={switched}
+      onChange={() =>
+        setTimeout(() => setSwitched((switched) => !switched), 300)
+      }
+      className={`${
+        !switched ? "bg-gray-900 dark:bg-gray-300/20" : "bg-gray-200"
+      }
           relative inline-flex h-[28px] w-[54px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-      >
-        <span className="sr-only">Use setting</span>
-        <span
-          aria-hidden="true"
-          className={`${!switched ? "translate-x-[26px]" : "translate-x-0"}
+    >
+      <span className="sr-only">Use setting</span>
+      <span
+        aria-hidden="true"
+        className={`${!switched ? "translate-x-[26px]" : "translate-x-0"}
             pointer-events-none inline-block h-6 w-6 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
-      </Switch>
-    </div>
+      />
+    </Switch>
   );
 }
---
+++ src/constants/navLinks.js
@@ -0,0 +1,7 @@
+import { HOME, PROJECTS, CONTACT } from "./routes";
+
+export const LINKS = [
+  { name: "About", to: HOME },
+  { name: "Projects", to: PROJECTS },
+  { name: "Contact", to: CONTACT },
+];
