f9e53a9dd25448996948f14a0f231344ff47ce84
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-07-02 01:22:36 +0200

    Fixed link hover and active bugs

--- components/navbar/DesktopNavLink.js
+++ components/navbar/DesktopNavLink.js
@@ -1,26 +1,27 @@
 import clsx from "clsx";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const DesktopNavLink = ({ href, text, ...rest }) => (
-  <li className="px-5 py-2">
-    <Link
-      // className={({ isActive = false }) => {
-      //   return clsx(
-      //     "underlined block whitespace-nowrap text-lg font-medium hover:text-black focus:text-black focus:outline-none dark:hover:text-white dark:focus:text-white dark:active:text-white",
-      //     {
-      //       active: isActive,
-      //       "text-gray-500 dark:text-gray-dark": !isActive,
-      //     }
-      //   );
-      // }}
-      href={href}
-      {...rest}
-    >
-      <a className="underlined block whitespace-nowrap text-lg font-medium hover:text-black focus:text-black focus:outline-none dark:hover:text-white dark:focus:text-white dark:active:text-white text-gray-500 dark:text-gray-dark">
-        {text}
-      </a>
-    </Link>
-  </li>
-);
+const DesktopNavLink = ({ href, text, ...rest }) => {
+  const router = useRouter();
+  const isActive = router.asPath === href;
+  return (
+    <li className="px-5 py-2">
+      <Link href={href} {...rest}>
+        <a
+          className={clsx(
+            "underlined block whitespace-nowrap text-lg font-medium hover:text-black focus:text-black focus:outline-none dark:hover:text-white dark:focus:text-white dark:active:text-white",
+            {
+              active: isActive,
+              "text-gray-500 dark:text-gray-dark": !isActive,
+            }
+          )}
+        >
+          {text}
+        </a>
+      </Link>
+    </li>
+  );
+};
 
 export default DesktopNavLink;
