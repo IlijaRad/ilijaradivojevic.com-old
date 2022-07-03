c042121cbb83bde0a58bdd7a596f0d70a4b3f282
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-07-03 16:52:55 +0200

    Made sitemap local and production version

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const path = require("path");
+
 import { projects } from "../constants/projectData";
 
 const Sitemap = () => {
@@ -7,34 +9,55 @@ const Sitemap = () => {
 
 export const getServerSideProps = async ({ res }) => {
   const BASE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL;
-  console.log(BASE_URL);
-  const staticPaths = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return ![
+  const env = process.env.NODE_ENV;
+  const isProd = env === "production";
+  const folderPath = isProd ? __dirname : "pages";
+  const removeExtension = isProd ? ".html" : ".js";
+
+  const ignoredPaths = isProd
+    ? ["404.html", "500.html", "project"]
+    : [
+        "_app.js",
+        "_document.js",
         "sitemap.xml.js",
         "404.js",
         "500.js",
-        "_app.js",
-        "_document.js",
         "project",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      return `${BASE_URL}/${staticPagePath.replace(".js", "")}`;
-    });
+        "_error.js",
+      ];
+  const staticPaths = fs.readdirSync(folderPath);
+
+  let staticPathsFiltered = staticPaths.filter((staticPage) => {
+    return !ignoredPaths.includes(staticPage);
+  });
+
+  let staticPathsFilteredByExtension = staticPathsFiltered.filter(
+    (staticPage) => staticPage.indexOf(removeExtension) !== -1
+  );
+
+  let staticPathsClean = staticPathsFilteredByExtension.map(
+    (staticPagePath) => {
+      return `${BASE_URL}/${staticPagePath.replace(removeExtension, "")}`;
+    }
+  );
+
+  staticPathsClean = staticPathsClean.map((staticPagePath) => {
+    return `${staticPagePath.replace("index", "")}`;
+  });
+
+  console.log(staticPathsClean);
 
   const dynamicPaths = projects.map((project) => {
     return `${BASE_URL}/project/${project.slug}`;
   });
 
-  const allPaths = [...staticPaths, ...dynamicPaths];
+  const allPaths = [...staticPathsClean, ...dynamicPaths];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${allPaths
         .map((url) => {
-          if (url !== BASE_URL + "/index") {
+          if (url !== BASE_URL + "/") {
             return `<url>
                 <loc>${url}</loc>
                 <lastmod>2022-03-07</lastmod>
